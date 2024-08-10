0d7c63ecc4c4a2e30bef46f50b75e5ebbffed8a1
Markus Ingerslev <markusolsen2004@gmail.com>
2024-08-10 22:27:48 +0200

    Discord Bot connected

---
+++ main.py
@@ -0,0 +1,49 @@
+# IMPORT DISCORD.PY. ALLOWS ACCESS TO DISCORD'S API.
+import discord
+
+# IMPORT THE OS MODULE.
+import os
+
+# IMPORT LOAD_DOTENV FUNCTION FROM DOTENV MODULE.
+from dotenv import load_dotenv
+
+# LOADS THE .ENV FILE THAT RESIDES ON THE SAME LEVEL AS THE SCRIPT.
+load_dotenv()
+
+# GRAB THE API TOKEN FROM THE .ENV FILE.
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+# DEFINE THE INTENTS. YOU CAN ADD MORE INTENTS IF YOU NEED THEM.
+intents = discord.Intents.default()
+intents.message_content = True  # Required to receive message content
+
+# GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
+bot = discord.Client(intents=intents)
+
+# EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
+@bot.event
+async def on_ready():
+    # CREATES A COUNTER TO KEEP TRACK OF HOW MANY GUILDS / SERVERS THE BOT IS CONNECTED TO.
+    guild_count = 0
+
+    # LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
+    for guild in bot.guilds:
+        # PRINT THE SERVER'S ID AND NAME.
+        print(f"- {guild.id} (name: {guild.name})")
+
+        # INCREMENTS THE GUILD COUNTER.
+        guild_count = guild_count + 1
+
+    # PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
+    print(f"JealousLover is in {guild_count} guilds.")
+
+# EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
+@bot.event
+async def on_message(message):
+    # CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
+    if message.content.lower() == "hello":
+        # SENDS BACK A MESSAGE TO THE CHANNEL.
+        await message.channel.send("hey dirtbag")
+
+# EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
+bot.run(DISCORD_TOKEN)
